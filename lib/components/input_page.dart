4203a2eb138e7ca24945805fb4bb06da2fe9b983
Chandra Vamshi Dasyam <chandravamshidasyam@Chandras-MacBook-Air.local>
2023-04-11 20:09:15 +0200

    small changes

--- lib/components/input_page.dart
+++ lib/components/input_page.dart
@@ -11,12 +11,52 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Calculator'),
-      ),
-      body: const Center(
-        child: Text('data'),
-      ),
+        appBar: AppBar(
+          title: const Text('BMI Calculator'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: const [
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                )
+              ],
+            )),
+            const Expanded(
+                child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            )),
+            Expanded(
+                child: Row(
+              children: const [
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                )
+              ],
+            ))
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
