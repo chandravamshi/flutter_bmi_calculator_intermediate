4fae498b3ac04ecc27a78b4ca14967d56b86c2e7
Chandra Vamshi Dasyam <chandravamshidasyam@Chandras-MacBook-Air.local>
2023-04-12 19:09:14 +0200

    created components for better code

--- lib/components/input_page.dart
+++ lib/components/input_page.dart
@@ -1,5 +1,8 @@
+import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -67,49 +70,4 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconContent extends StatelessWidget {
-  const IconContent({
-    super.key,
-    required this.icon, required this.label,
-  });
 
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        const SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          label,
-          style: const TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  const ReusableCard(
-      {super.key, required this.colour, this.cardChild});
-  final Color colour;
-  final Widget? cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: colour, borderRadius: BorderRadius.circular(10.0)),
-      child: cardChild,
-    );
-  }
-}
